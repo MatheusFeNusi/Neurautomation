c6e099bcd6e8e35a1c3abf2bda2ed1a1b5cb4c6b
MatheusFeNusi <matheusferreiragu2@gmail.com>
2026-09-16 03:38:47 -0300

    Add impact.com tracking script to homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+const IMPACT_STAT_SNIPPET = `(function(i,m,p,a,c,t){c.ire_o=p;c[p]=c[p]||function(){(c[p].a=c[p].a||[]).push(arguments)};t=a.createElement(m);var z=a.getElementsByTagName(m)[0];t.async=1;t.src=i;z.parentNode.insertBefore(t,z)})('https://utt.impactcdn.com/P-A7781791-d359-4b7b-b3b4-34371909d4161.js','script','impactStat',document,window);impactStat('transformLinks');impactStat('trackImpression');`;
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,7 +26,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="pt-BR"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col bg-black text-white">{children}</body>
+      <body className="min-h-full flex flex-col bg-black text-white">
+        <script dangerouslySetInnerHTML={{ __html: IMPACT_STAT_SNIPPET }} />
+        {children}
+      </body>
     </html>
   );
 }
